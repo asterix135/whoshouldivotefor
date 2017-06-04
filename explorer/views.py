37fca7192125b18b5c65b48f3d07a6bf4a019c05
asterix135 <chgraham@gmail.com>
2017-06-04 09:47:04 -0400

    added model for testing

--- explorer/views.py
+++ explorer/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 def index(request):
     # return HttpResponse('<h2>Home Page for WhoShouldIVoteFor.ca</h2>')
     return render(request, 'explorer/index.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'explorer/db.html', {'greetings': greetings})
