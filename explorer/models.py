80f7d3b93ee7db40c10e7e6f8d0fb9556e5627f8
asterix135 <chgraham@gmail.com>
2017-06-07 18:59:15 -0400

    changed views to class

--- explorer/models.py
+++ explorer/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+class Polity(models.Model):
+    """
+    contains information on political entities
+    """
+    name = models.CharField(max_length=255)
+    parent_polity = models.ForeignKey('self', on_delete=models.CASCADE)
+    web_site = models.URLField()
+    election_website = models.URLField()
+    next_election = models.DateField()
+    num_representatives = models.IntegerField()
+    num_wards = models.IntegerField()
+    separate_executive = models.BooleanField()  # usually refers to mayor
+    notes = models.TextField()
--- explorer/views.py
+++ explorer/views.py
@@ -2,22 +2,14 @@ import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 from .models import *
 
-def index(request):
-    context = {
-        'greeting': 'Hello! ' * 2,
-    }
-    # return HttpResponse('<h2>Home Page for WhoShouldIVoteFor.ca</h2>')
-    return render(request, 'explorer/index.html', context)
+class Index(TemplateView):
+    template_name = 'explorer/index.html'
 
-
-# def db(request):
-#
-#     greeting = Greeting()
-#     greeting.save()
-#
-#     greetings = Greeting.objects.all()
-#
-#     return render(request, 'explorer/db.html', {'greetings': greetings})
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        context['greeting'] = 'Hello! ' * 2
+        return context
