07f9105d67eb7e7980dbf7f9384ca2529a62757e
asterix135 <chgraham@gmail.com>
2017-06-23 19:44:25 -0400

    Added models for main system logic

--- explorer/models.py
+++ explorer/models.py
@@ -22,3 +22,78 @@ class Polity(models.Model):
     creator = models.ForeignKey('auth.User', related_name='polities',
                                 on_delete=models.SET_NULL,
                                 blank=True, null=True)
+
+
+class Election(models.Model):
+    """
+    Election details
+    """
+    polity = models.ForeignKey(Polity,
+                               on_delete=models.CASCADE)
+    vote_date = models.DateField()
+    start_date = models.DateField(null=True, blank=True)
+
+
+class District(models.Model):
+    """
+    Information on ridings/wards/districts
+    """
+    polity = models.ForeignKey(Polity,
+                               on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    number = models.CharField(max_length=25,
+                              null=True, blank=True)
+    num_reps = models.IntegerField(default=1,)
+    is_whole_polity = models.BooleanField(default=False,)
+
+
+class Politician(models.Model):
+    """
+    Information on individuals running for office
+    """
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    web_site = models.URLField(blank=True, null=True)
+    twitter_handle = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=30, blank=True, null=True)
+
+
+class Candidate(models.Model):
+    """
+    Information on Candidates
+    """
+    politician = models.ForeignKey(Politician,
+                                   on_delete=models.CASCADE)
+    election = models.ForeignKey(Election,
+                                 on_delete=models.CASCADE)
+    district = models.ForeignKey(District,
+                                 on_delete=models.CASCADE)
+    incumbent = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('politician', 'election', 'district')
+
+
+class Poll(models.Model):
+    election = models.OneToOneField(Election)
+    name = models.CharField(max_length=100, default='default')
+
+
+class Question(models.Model):
+    poll = models.ManyToManyField(Poll)
+    question = models.TextField()
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    agreement = models.IntegerField(default=5)
+    importance = models.IntegerField(default=5)
+
+
+class CandidatePosition(Answer):
+    candidate = models.ForeignKey(Candidate)
+
+
+class PublicAnswer(Answer):
+    ip_address = models.GenericIPAddressField()
