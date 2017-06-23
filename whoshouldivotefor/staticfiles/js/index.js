1b79205b316449fc61d4268d9cc40fcdb867f727
asterix135 <chgraham@gmail.com>
2017-06-23 19:43:58 -0400

    React experiments

--- whoshouldivotefor/staticfiles/js/index.js
+++ whoshouldivotefor/staticfiles/js/index.js
@@ -2,14 +2,26 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
-var Hello = createReactClass({
+var ProvinceSelector = createReactClass({
   render: function(){
-    return (
-      <h1>
-      Hello, React!
-      </h1>
+    return(
+      <input class="form-control" name="province" placeholder="province name"></input>
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('container'));
+var QuizApp = createReactClass({
+  render: function(){
+    return (
+      <div>
+        <h1 class="display-3">
+        Where do you live?
+        </h1>
+        <ProvinceSelector />
+      </div>
+    )
+  }
+})
+
+// ReactDOM.render(<Hello />, document.getElementById('container'));
+ReactDOM.render(<QuizApp />, document.getElementById('container'));
